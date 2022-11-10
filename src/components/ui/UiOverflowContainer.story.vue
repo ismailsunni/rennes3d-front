b854cb40f46f503ea10e67516e0c4153085757ca
Ismail Sunni <imajimatika@gmail.com>
2022-11-10 12:51:39 +0700

    WIP: sample usage of swiperjs.

--- src/components/ui/UiOverflowContainer.story.vue
+++ src/components/ui/UiOverflowContainer.story.vue
@@ -7,19 +7,19 @@ import UiOverflowContainer from './UiOverflowContainer.vue'
     title="Overflow Container"
     :layout="{
       type: 'grid',
-      width: 500,
+      width: 300,
     }"
     group="ui-base"
   >
     <Variant title="Test">
       <template #default>
         <UiOverflowContainer>
-          <div class="flex flex-row items-start w-96 gap-2">
+          <!-- <div class="flex flex-row items-start w-96 gap-2">
             <div class="h-10 w-80 border-2 flex-none">1</div>
             <div class="h-10 w-80 border-2 flex-none">2</div>
             <div class="h-10 w-80 border-2 flex-none">3</div>
             <div class="h-10 w-80 border-2 flex-none">4</div>
-          </div>
+          </div> -->
         </UiOverflowContainer>
       </template>
     </Variant>
--- src/components/ui/UiOverflowContainer.vue
+++ src/components/ui/UiOverflowContainer.vue
@@ -1,6 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+// import required modules
+import { Navigation } from 'swiper'
+</script>
+
 <template>
-  <div class="overflow-auto scrollbar-hide">
-    <slot></slot>
-  </div>
+  <swiper :navigation="true" :modules="[Navigation]" class="mySwiper">
+    <swiper-slide>
+      <img
+        className="object-fill w-full h-96"
+        src="https://cdn.pixabay.com/photo/2022/03/20/15/40/nature-7081138__340.jpg"
+        alt="image slide 1"
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <img
+        className="object-fill w-full h-96"
+        src="https://cdn.pixabay.com/photo/2022/07/24/17/55/wind-energy-7342177__340.jpg"
+        alt="image slide 2"
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <img
+        className="object-fill w-full h-96"
+        src="https://cdn.pixabay.com/photo/2022/07/26/03/35/jogger-7344979__340.jpg"
+        alt="image slide 3"
+      />
+    </swiper-slide>
+  </swiper>
 </template>
