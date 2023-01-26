ee8de9e85e741c0f285e46f1d6912be1019011a9
Ismail Sunni <imajimatika@gmail.com>
2023-01-26 22:29:52 +0700

    Add modal component.

---
+++ src/components/ui/UiModal.story.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import UiModal from './UiModal.vue'
+</script>
+
+<template>
+  <Story
+    title="Modal"
+    :layout="{
+      type: 'grid',
+      width: 550,
+    }"
+    group="ui-base"
+  >
+    <template #default>
+      <UiModal>
+        <template #header
+          >Politique de Protection des Données Personnelles</template
+        >
+        <template #body
+          >Conformément au Règlement général sur la protection des données
+          (RGPD) (UE) n°2016/679 du 27 avril 2016 et à la loi n°78-17 du 6
+          janvier 1978 modifiée relative à l'informatique, aux fichiers et aux
+          libertés, vous pouvez accéder aux données vous concernant ou demander
+          leur effacement. Vous disposez également d'un droit d’opposition, d’un
+          droit de rectification et d’un droit à la limitation du traitement de
+          vos données. Pour exercer ces droits ou pour toute question sur le
+          traitement de vos données, vous pouvez contacter le Délégué à la
+          protection des données de Rennes (Ville et Métropole) via le
+          formulaire de contact "e-démarches" du site internet
+          (http://metropole.rennes.fr) ou par voie postale à adresser à Madame
+          la Présidente de Rennes Métropole - A l'attention du Délégué à la
+          protection des données, 4 avenue Henri Fréville - CS93111 - 35031
+          Rennes Cedex Plus d’informations sur : https://www.cnil.fr</template
+        >
+      </UiModal>
+    </template>
+  </Story>
+</template>
---
+++ src/components/ui/UiModal.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import deleteSquare from '@/assets/icons/delete-square.svg'
+import UiButton from '@/components/ui/UiButton.vue'
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-center items-start py-11 px-6 gap-8 bg-white shadow-lg rounded-lg w-[460px]"
+  >
+    <div class="flex justify-end items-start p-0 gap-2.5">
+      <h2 class="font-dm-sans font-bold text-2xl flex items-center">
+        <slot name="header">Header</slot>
+      </h2>
+      <a
+        role="button"
+        class="flex items-center pt-0 pr-0 pb-1 pl-0 gap-2 w-4 h-5"
+      >
+        <img class="w-4 h-4" :src="deleteSquare" />
+      </a>
+    </div>
+
+    <div class="font-dm-sans font-normal text-sm text-neutral-700">
+      <slot name="body">Lorem ipsum</slot>
+    </div>
+    <UiButton
+      class="ui-btn-regular-size ui-btn-primary"
+      :disabled="false"
+      :active="true"
+    >
+      Fermer
+    </UiButton>
+  </div>
+</template>
